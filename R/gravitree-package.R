0190a52d744e13d11ddbda9dd4bc64d86d9d334e
Simon Garnier <garnier@njit.edu>
2024-07-13 17:19:34 +0200

    Add data.

--- R/gravitree-package.R
+++ R/gravitree-package.R
@@ -21,3 +21,37 @@
 #' @importFrom Rcpp evalCpp
 #' @useDynLib gravitree, .registration = TRUE
 "_PACKAGE"
+
+
+#' @title Four Gaussian Clusters
+#'
+#' @description Matrix of 2-dimensional points forming four Gaussian clusters.
+#'
+#' @name four_Gaussians
+#'
+#' @docType data
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(four_Gaussians)
+#' plot(four_Gaussians, pch = 19)
+#'
+NULL
+
+
+#' @title Three Gaussian Clusters
+#'
+#' @description Matrix of 2-dimensional points forming three Gaussian clusters.
+#'
+#' @name three_Gaussians
+#'
+#' @docType data
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(three_Gaussians)
+#' plot(three_Gaussians, pch = 19)
+#'
+NULL
